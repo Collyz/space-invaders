aa6a146265d4bd6eaa5740ce690b85e98da9108a
Collyz <m.mowla03151@gmail.com>
2025-08-13 16:15:17 -0400

    New start menu screen using a sprite for text and diff scaling

--- scenes/start_menu/start_menu.gd
+++ scenes/start_menu/start_menu.gd
@@ -1,41 +1,39 @@
 extends Control
 
 @onready var start_menu_bg = $StartMenuBg
-@onready var title_label = $TitleLabel
+@onready var title_label = $VBoxContainer/TitleLabel
+@onready var start_sprite = $Sprite2D
 @onready var start_button = $Button
-@onready var start_label = $StartLabel
 @onready var screen_size = get_viewport_rect().size
 @onready var screen_height = screen_size.y
 @onready var screen_width = screen_size.x
 
-var time_accum := 0.0
+var shader_time := 0.0
 var scale_speed := 4
 
 func _ready():
-	# Set the background and button size to be the entire screen
-	start_menu_bg.size = get_viewport_rect().size
-	start_button.size = get_viewport_rect().size
-	reposition_title()
-	reposition_start_label()
+	resize_start_sprite()
+	reposition_start_sprite()
 	
-	start_label.pivot_offset = start_label.size / 2
-	#start_label.pivot_offset = start_label.pivot_offset.round() # pixel align
 	
 func _process(delta):
-	# Pulse size (breathing effect)
-	time_accum += delta
-	var scale_factor = 1.0 + 0.05 * sin(time_accum * scale_speed) * 4 # ±5% scale
-	start_label.scale = Vector2(scale_factor, scale_factor)
+	pass
 
-func reposition_title() -> void:
-	# Move the title label to be center aligned and the y pos to be 1/6 of the height of the screen
-	title_label.position.x = screen_width/2 - title_label.size.x/2
-	title_label.position.y = screen_height/6
+func reposition_start_sprite() -> void:
+	start_sprite.position.x = screen_width/2 
+	start_sprite.position.y = screen_height/2
+	
+func resize_start_sprite() -> void:
+	var target_width = screen_width * 0.3   # half the screen width
+	var target_height = screen_height * 0.3 # half the screen height
 
-func reposition_start_label() -> void:
-	# Move the start label to the center of the screen
-	start_label.position.x = screen_width/2 - start_label.size.x/2
-	start_label.position.y = screen_height/2
+	var scale_x = target_width / start_sprite.texture.get_width()
+	var scale_y = target_height / start_sprite.texture.get_height()
+
+	var uniform_scale = min(scale_x, scale_y)
+	start_sprite.scale = Vector2(uniform_scale, uniform_scale)
+
+
 
 func _on_button_pressed():
 	var msg = 'Hello World!'
---
+++ scenes/start_menu/title_label.gd
@@ -0,0 +1,15 @@
+extends Label
+
+func _ready():
+	# Initial adjustment on ready
+	_adjust_font_size()
+
+func _on_viewport_resized():
+	# Adjust font size when viewport resizes
+	_adjust_font_size()
+
+func _adjust_font_size():
+	var viewport_width = get_viewport().size.x
+	# Calculate desired font size based on a ratio of viewport height
+	var new_font_size = int(viewport_width / 10.0) # Adjust this ratio as needed
+	add_theme_font_size_override("font_size", new_font_size)

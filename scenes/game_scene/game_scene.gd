93016557b8f0b8d443ca432dcd20e515093706fb
Collyz <m.mowla03151@gmail.com>
2025-08-15 21:29:05 -0400

    Space enemies using better variation

--- scenes/game_scene/game_scene.gd
+++ scenes/game_scene/game_scene.gd
@@ -10,7 +10,7 @@ extends Node2D
 @onready var screen_size = get_viewport_rect().size
 
 var enemies = []
-var enemies_per_row = 5
+var enemies_per_row = 10
 var enemies_per_col = 3
 
 var player_count: int = 2
@@ -58,36 +58,45 @@ func _instantiate_simple_enemy() -> SimpleEnemy:
 	return simple_enemy.instantiate()
 	
 func spawn_enemies() -> void:
-	enemies_holder.position = Vector2(screen_size.x/2, screen_size.y/8)
-	var width = screen_size.x
-	var arb_dist = 150
-	var next_spawn_loc = Vector2.ZERO
-	next_spawn_loc.y = 50
+	enemies_holder.position = Vector2(screen_size.x / 2, screen_size.y / 8)
+	var screen_width = screen_size.x
+	var padding = 75
+	var enemy_width = 128
+	var enemy_height = 64
 	
 	# Reset the enemies array
 	enemies = []
 	for i in range(enemies_per_col):
 		enemies.append([])
 	
-	# Instantiate all of the enemies
+	# Instantiate enemies
 	for i in range(enemies_per_col):
 		for j in range(enemies_per_row):
 			enemies[i].append(_instantiate_simple_enemy())
+	
+	# Calculate spacing
+	var total_enemy_width = enemies_per_row * enemy_width
+	var available_gap_space = screen_width - (2 * padding) - total_enemy_width
+	var gap_size = available_gap_space / float(enemies_per_row - 1)
+	
+	# Total width of the whole enemy block
+	var block_width = total_enemy_width + (enemies_per_row - 1) * gap_size
+	
+	# Leftmost x (relative to center, so start at -block_width/2)
+	var start_x = -block_width / 2
+	
+	# Place enemies
+	for row in range(enemies.size()):
+		for col in range(enemies[row].size()):
+			var enemy: SimpleEnemy = enemies[row][col]
+			enemies_holder.add_child(enemy)
 			
-	# Add them to the scene tree at certain positions
-	for i in range(enemies.size()):
-		for j in range(enemies[0].size()):
-			#print(enemies[i][j])
-			var enemy: SimpleEnemy = enemies[i][j]
-			enemies_holder.add_child(enemies[i][j])
-			enemy.position.x = -width / 2 + enemy.sprite_width + arb_dist + next_spawn_loc.x
-			enemy.position.y = next_spawn_loc.y
-			next_spawn_loc.x += arb_dist + enemy.sprite_width
-		next_spawn_loc.x = 0
-		next_spawn_loc.y += 150
+			var x = start_x + col * (enemy_width + gap_size)
+			var y = row * enemy_height
+			enemy.position = Vector2(x, y)
 	
 	# Add visible on screen notifier to end of first and last row of enemies
-	var padding = 100
+	var notifier_padding = 50
 	var left_most_enemy_pos = enemies[0][0].position
 	var right_most_enemy_pos = enemies[0][enemies[0].size()-1].position
 	
@@ -95,9 +104,9 @@ func spawn_enemies() -> void:
 	var right_notifier = VisibleOnScreenNotifier2D.new()
 	# Add notifiers to scene tree
 	enemies_holder.add_child(left_notifier)
-	left_notifier.position.x = left_most_enemy_pos.x - padding
+	left_notifier.position.x = left_most_enemy_pos.x - notifier_padding
 	enemies_holder.add_child(right_notifier)
-	right_notifier.position.x = right_most_enemy_pos.x + padding
+	right_notifier.position.x = right_most_enemy_pos.x + notifier_padding
 	# Connect the signals
 	left_notifier.screen_exited.connect(_on_left_group_off_screen)
 	right_notifier.screen_exited.connect(_on_right_group_off_screen)
